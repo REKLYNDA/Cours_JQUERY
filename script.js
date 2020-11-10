4f09a0ab842bb6b4b967d157caf259d09a32c181
clone_JS <relykaa@gmail.com>
2020-11-10 13:22:57 +0100

    Ajout du temps et de callbacks comme param dans la fonction hide; ajout en cascade

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 $(function(){
 	
-	// $("p").hide();
-	$('#p1').on({  // Fonctionnement de "on" avec l'objet entre parenthèse 
-		
-		mouseenter: function(){ // si la souriis entre dans le  parag 1 on cache le parag 4
-			$("#p4").hide();
-		},
-	 
-		mouseleave: function(){  // si la souris sort du parag 1 on montre le parag 4
-			$("#p4").show();
-   	    },
-		
-		click: function(){  // si on clique sur le  parag 1 on cache le parag 2
-			$("#p2").hide();
-		}
-	});
 	
+	$('#p1').click( function(){ 
+		
+		$("#p2").hide(2000, function(){
+			$("#p3").hide(3000, function(){
+				$("#p4").hide(3000);
+			});
+		});
+	});
 });
