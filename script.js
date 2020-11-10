7cc87f4ce81ca90ac0e00707216c71cea6bb347d
clone_JQUERY <relykaa@gmail.com>
2020-11-10 15:46:51 +0100

    Animation à la chaine en mettant un point + une new fct à la suite de notre 1ere fct et ainsi de suite

--- script.js
+++ script.js
@@ -2,15 +2,9 @@ $(function(){
 	
 	$("#start").click(function(){
 		
-		$("div").animate({left:"600px"}, 5000);
-		$("div").animate({opacity:0.2}, 5000);
-		$("div").animate({width:"+=100px"}, 5000);
-		$("div").animate({height:"+=100px"}, 5000);
+		$("div").slideUp(5000).slideDown(3000).fadeOut(3000).fadeIn(3000);
+	
 	});
 	
-	
-	$("#stop").click(function(){
-		$("div").stop(true,true); // arg 1 stop toutes les animations ; arg 2 va jusqu'à la fin de l'animation courante
-		
-	});
+
 });
