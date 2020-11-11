18e832b0fae35392da6cf6c87be6da98332a2c82
clone_JQUERY <relykaa@gmail.com>
2020-11-11 16:19:39 +0100

    Fonctions sur l'arborescence : parent (le parent direct), parents(tous les parents), children (les enfants), first (le premier parag for example), last(le dernier), eq(index) (le parag d'index x), siblings( les frères), next(le frère suivant), filter(attr) (les éléments avec cet attribut), not(attr) (les éléments qui n'ont pas cet attribut), find(elt) (tous les éléments de type elt)

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
 $(function(){
 	
-	$("#but1").click(function(){
+	$("#start").click(function(){
 		
-		$("#but2").width(150);
-		$("#but2").height(150);
-		
-	});
-	
-	/*
-		
-		$("#start").click(function(){
-		$("#p1, #p2").addClass("red  bold");
+		$("p").not(".special").css("border-color", "green");
 		
 	});
-	
-
-		$("#but1").click(function(){
-		$("#p1").togglelass("red");
-		
-		});
-
-
-		$("#but1").click(function(){
-		$("#p1").css("color","red");
-		
-		});
-
-
-
-		$("#p1").css({"color": "red",
-					 "font-weight": "bold",
-					  "background-color": "yellow"});
-
-
-
-		$("#but2").width(150);
-		$("#but2").height(150);
-	
 });
+	
