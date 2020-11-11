2e1d946eb8c62999ad970813411276e14f0121fa
clone_JQUERY <relykaa@gmail.com>
2020-11-11 13:58:39 +0100

    rebase de mes commits

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 $(function(){
-		
-	$("#start").click(function(){
-		
-		$("div").slideUp(5000).slideDown(3000).fadeOut(3000).fadeIn(3000);
 	
+	$("#but1").click(function(){
+		
+		$("#but2").width(150);
+		$("#but2").height(150);
+		
 	});
+	
+	/*
+		
+		$("#start").click(function(){
+		$("#p1, #p2").addClass("red  bold");
+		
+	});
+	
 
+		$("#but1").click(function(){
+		$("#p1").togglelass("red");
+		
+		});
+
+
+		$("#but1").click(function(){
+		$("#p1").css("color","red");
+		
+		});
+
+
+
+		$("#p1").css({"color": "red",
+					 "font-weight": "bold",
+					  "background-color": "yellow"});
+
+
+
+		$("#but2").width(150);
+		$("#but2").height(150);
+	
 });
